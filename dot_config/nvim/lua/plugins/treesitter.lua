c3c0e49cc8bb03f4da4049f6950334d1f2f58af8
Hunter Jennings <jenningsdhunter@gmail.com>
2025-12-08 18:20:03 -0500

    feat(nvim): Add treesitter textobjects plugin
    - Add nvim-treesitter-textobjects plugin for enhanced code navigation
    - Configure text object selections for functions, classes, parameters, conditionals, and loops
    - Add keybindings for moving between code structures (]f, [f, ]], [[, etc.)
    - Enable parameter swapping with <leader>sa and <leader>sA
    - Add LSP interop with peek definition for functions and classes

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -21,4 +21,75 @@ return {
 			max_lines = 1,
 		},
 	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							-- Functions
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							-- Classes
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+							-- Parameters/arguments
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							-- Conditionals
+							["ai"] = "@conditional.outer",
+							["ii"] = "@conditional.inner",
+							-- Loops
+							["al"] = "@loop.outer",
+							["il"] = "@loop.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+							["]]"] = "@class.outer",
+							["]a"] = "@parameter.inner",
+						},
+						goto_next_end = {
+							["]F"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[["] = "@class.outer",
+							["[a"] = "@parameter.inner",
+						},
+						goto_previous_end = {
+							["[F"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>sa"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>sA"] = "@parameter.inner",
+						},
+					},
+					lsp_interop = {
+						enable = true,
+						border = "rounded",
+						floating_preview_opts = {},
+						peek_definition_code = {
+							["<leader>df"] = "@function.outer",
+							["<leader>dF"] = "@class.outer",
+						},
+					},
+				},
+			})
+		end,
+	},
 }
